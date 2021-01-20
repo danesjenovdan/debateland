86f1bc34dee29154388dda7101e6ef750afc52e5
muki <filipdobranic@gmail.com>
2021-01-20 10:37:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to run the dev server?
+
+`[sudo] docker-compose up`
+
+# How to run migrations?
+
+`[sudo] docker-compose exec web bash`
---
+++ debateland/home/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from novice.models import NovicaPage
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Image
+
+
+@register_setting
+class OgSettings(BaseSetting):
+    og_title = models.CharField(max_length=255)
+    og_description = models.CharField(max_length=255)
+    og_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+
+    panels = [
+        FieldPanel('og_title'),
+        FieldPanel('og_description'),
+        ImageChooserPanel('og_image'),
+    ]
+
+
+class HomePage(Page):
+    intro_text = RichTextField(blank=True, null=True)
+    description_text = RichTextField(blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro_text', classname="full"),
+        FieldPanel('description_text', classname="full")
+    ]
+
+    parent_page_types = []
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        novice = NovicaPage.objects.all().live().order_by('-first_published_at')
+        context['novice'] = novice
+        return context
+
+
+class GenericPage(Page):
+    body = StreamField([
+        ('heading', blocks.StructBlock([
+            ('part_one', blocks.CharBlock(required=False)),
+            ('part_two', blocks.CharBlock(required=False)),
+            ('intro_text', blocks.RichTextBlock(required=False)),
+        ], icon='title')),
+        ('paragraph', blocks.RichTextBlock()),
+        ('DonationEmbed', blocks.StaticBlock(admin_text='Donation embed'))
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
