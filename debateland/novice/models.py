5fbf7fd99743634148c21bd4ca5815731b472ce6
Patricija Brečko <patricija.brecko@gmail.com>
2021-01-25 14:07:43 +0100

    novica django model & html template

--- debateland/novice/models.py
+++ debateland/novice/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 
 
 class NovicaPage(Page):
     date = models.DateField()
-    body = RichTextField(blank=False, null=False, default='')
+    preview_text = RichTextField(blank=False, null=False, default='')
+    body = StreamField([
+            ('paragraph', blocks.RichTextBlock()),
+        ])
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
-        FieldPanel('body', classname="full")
+        FieldPanel('preview_text', classname="full"),
+        StreamFieldPanel('body', classname="full")
     ]
