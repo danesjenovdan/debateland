eb58bcdac71be1b390faaf73f03e237e5ad26c80
muki <filipdobranic@gmail.com>
2021-01-28 09:37:05 +0100

    add DB_FOLDER env variable

--- debateland/debateland/settings/base.py
+++ debateland/debateland/settings/base.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'debateland.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.getenv('DB_FOLDER', BASE_DIR), 'db.sqlite3'),
     }
 }
 
