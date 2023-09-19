d8cd0319448bd11191204744160524bf35f73ee3
Patricija Brečko <patricija@danesjenovdan.si>
2023-09-19 11:30:24 +0200

    cleanup

--- debateland/home/urls.py
+++ debateland/home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path
 
-from .views import ExerciseListPartialView, ExerciseListView, ExerciseView
+from .views import ExerciseListView, ExerciseView
 
 urlpatterns = [
     path("<int:id>-<slug:slug>/", ExerciseView.as_view(), name="exercise"),
-    path("filter-list", ExerciseListPartialView.as_view(), name="exercise_list_partial"),
     re_path("", ExerciseListView.as_view(), name="exercise_list"),
 ]
--- debateland/home/wagtail_hooks.py
+++ debateland/home/wagtail_hooks.py
@@ -36,7 +36,6 @@ from .models import (
 )
 
 
-
 class NewTabExternalLinkHandler(LinkHandler):
     identifier = 'external'
 
@@ -92,133 +91,15 @@ def register_extra_rich_text_features(features):
     })
 
 
-
-# --- HELPERS
-
-
-class CopyView(InstanceSpecificView):
-    page_title = gettext_lazy("Copy")
-
-    def check_action_permitted(self, user):
-        return self.permission_helper.user_can_edit_obj(user, self.instance)
-
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        if not self.check_action_permitted(request.user):
-            raise PermissionDenied
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_meta_title(self):
-        return _("Confirm copying of %(object)s") % {"object": self.verbose_name}
-
-    def confirmation_message(self):
-        return _("Are you sure you want to copy this %(object)s?") % {
-            "object": self.verbose_name
-        }
-
-    def copy_instance(self):
-        return self.model_admin.copy(self.instance)
-
-    def post(self, request, *args, **kwargs):
-        msg = _("%(model_name)s '%(object)s' copied.") % {
-            "model_name": self.verbose_name,
-            "object": self.instance,
-        }
-        with transaction.atomic():
-            new_instance = self.copy_instance()
-        messages.success(request, msg)
-        return redirect(self.url_helper.get_action_url("edit", quote(new_instance.pk)))
-
-    def get_template_names(self):
-        ma = self.model_admin
-        if hasattr(ma, "copy_template_name") and ma.copy_template_name:
-            return ma.copy_template_name
-        return ma.get_templates("copy")
-
-
-class ModelAdminCopyMixin:
-    copy_view_class = CopyView
-
-    def copy(self, instance):
-        raise NotImplementedError(
-            "The copy() method must be implemented for each model admin"
-        )
-
-    def copy_view(self, request, instance_pk):
-        kwargs = {"model_admin": self, "instance_pk": instance_pk}
-        view_class = self.copy_view_class
-        return view_class.as_view(**kwargs)(request)
-
-    def get_admin_urls_for_registration(self, parent=None):
-        urls = super().get_admin_urls_for_registration()
-
-        urls = urls + (
-            re_path(
-                self.url_helper.get_action_url_pattern("copy"),
-                self.copy_view,
-                name=self.url_helper.get_action_url_name("copy"),
-            ),
-        )
-
-        return urls
-
-
-# class ExerciseButtonHelper(ButtonHelper):
-#     view_button_classnames = ["button", "button-secondary", "button-small"]
-
-#     def view_button(self, obj):
-#         return {
-#             "url": reverse("lesson", kwargs={"id": obj.id, "slug": slugify(obj.title)}),
-#             "label": _("View live"),
-#             "classname": self.finalise_classname(self.view_button_classnames),
-#             "title": _("View live"),
-#         }
-
-#     def copy_button(self, pk):
-#         return {
-#             "url": self.url_helper.get_action_url("copy", quote(pk)),
-#             "label": _("Copy"),
-#             "classname": self.finalise_classname(self.view_button_classnames),
-#             "title": _("Copy this %(object)s") % {"object": self.verbose_name},
-#         }
-
-#     def get_buttons_for_obj(
-#         self,
-#         obj,
-#         exclude=None,
-#         classnames_add=None,
-#         classnames_exclude=None,
-#     ):
-#         btns = super().get_buttons_for_obj(
-#             obj,
-#             exclude,
-#             classnames_add,
-#             classnames_exclude,
-#         )
-
-#         ph = self.permission_helper
-#         usr = self.request.user
-#         pk = getattr(obj, self.opts.pk.attname)
-
-#         if "copy" not in (exclude or []) and ph.user_can_edit_obj(usr, obj):
-#             btns.append(self.copy_button(pk))
-
-#         if "view" not in (exclude or []):
-#             btns.append(self.view_button(obj))
-
-#         return btns
-
-
 # --- MODEL ADMIN
 
 
-class ExerciseAdmin(ModelAdminCopyMixin, ModelAdmin):
+class ExerciseAdmin(ModelAdmin):
     model = Exercise
     menu_icon = "clipboard-list"
     list_display = ("title", "language", "topic")
     list_filter = ("language", "topic")
     search_fields = ("title")
-    # button_helper_class = ExerciseButtonHelper
 
 
 exercise_admin = ExerciseAdmin()
@@ -256,46 +137,3 @@ class ExercisesGroup(ModelAdminGroup):
 
 
 modeladmin_register(ExercisesGroup)
-
-
-# --- HOOKS
-
-
-@hooks.register("insert_global_admin_css")
-def global_admin_css():
-    return format_html(
-        '<link rel="stylesheet" href="{}">', static("css/custom_admin.css")
-    )
-
-
-class UserbarEditExerciseItem:
-    def render(self, request):
-        if "exercise" not in request.resolver_match.url_name:
-            return ""
-
-        try:
-            exercise_id = request.resolver_match.kwargs["id"]
-        except KeyError:
-            return ""
-
-        if not exercise_id:
-            return ""
-
-        edit_url = exercise_admin.url_helper.get_action_url("edit", exercise_id)
-        return format_html(
-            """
-            <li class="w-userbar__item " role="presentation">
-                <a href="{}" target="_parent" role="menuitem" tabindex="-1">
-                    <svg class="icon icon-edit w-action-icon" aria-hidden="true"><use href="#icon-edit"></use></svg>
-                    Edit this lesson
-                </a>
-            </li>
-            """,
-            edit_url,
-        )
-
-
-@hooks.register("construct_wagtail_userbar")
-def add_edit_lesson_item(request, items):
-    return items.insert(1, UserbarEditExerciseItem())
-
